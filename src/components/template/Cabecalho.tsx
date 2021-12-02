8e493f89f9c0cf74840234a623bee1a4dd352ce7
Thiago <thiagooffice2@gmail.com>
2021-12-01 22:11:47 -0300

    Criando Botão trocar para modo dark

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,4 +1,6 @@
-import MenuLateral from "./MenuLateral";
+import useAppData from "../../data/hook/useAppData";
+import BotaoAlternarTema from "./BotaoAlternarTema";
+
 import Titulo from "./Titulo";
 
 interface CabecalhoProps{
@@ -7,9 +9,15 @@ interface CabecalhoProps{
 }
 
 export default function Cabecalho(props: CabecalhoProps){
+
+    const {tema, alternarTema }=useAppData()
+
     return(
-        <div>
+        <div className={`flex`}>
            <Titulo titulo={props.titulo} subtitulo={props.subtitulo}/>
+           <div className={`flex flex-grow justify-end`}>
+            <BotaoAlternarTema tema="dark" alternarTema={alternarTema}/>
+           </div>
         </div>
     )
 }
