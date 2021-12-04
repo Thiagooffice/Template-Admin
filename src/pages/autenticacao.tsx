a56d4eca7625ff9d482d8159a4462e2d078ab6ab
Thiago <thiagooffice2@gmail.com>
2021-12-03 22:09:24 -0300

    Criando tela de autenticação 1

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -8,11 +8,30 @@ export default function Autenticacao(){
     const [email,setEmail] = useState("")
     const [senha,setSenha] = useState("")
 
+        function submeter(){
+            if(modo === "login"){
+                console.log("Login")
+            }else{
+                console.log("cadastrar")
+            }
+        }
 
 
     return(
-        <div>
-            <h1>Autenticacao</h1>
+        <div className={`
+        flex flex-col h-sreen items-center justify-center
+        `}>
+        
+        
+
+        <div className={`
+        w-1/2
+        `}>
+            <h1 className={`
+            text-xl font-bold mb-5`}>
+                {modo === "login" ? "Entre com Sua Conta" : "Cadastre-se na Plataforma"}
+
+            </h1>
             <AuthInput 
             label="Email"
             tipo="email"
@@ -27,6 +46,25 @@ export default function Autenticacao(){
             valorMudou={setSenha}
             obrigatorio
             />
+
+            <button className={`
+            w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-full px-4 py-3 mt-6
+            `} onClick={submeter}>
+            {modo === "login" ? "Entrar" : "Cadastrar"}
+
+            </button>
+
+            <hr  className={`
+            my-6 border-gray-300 h-full`}/>
+            
+            <button className={`
+            w-full bg-red-500 hover:bg-red-400 text-white rounded-full px-4 py-3
+            `} onClick={submeter}>
+            Entrar com o Google
+            
+
+            </button>
+        </div>
         </div>
     )
 }
