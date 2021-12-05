27507ee6d95fdab5dd97fd2b2524e50e0db92cb9
Thiago <thiagooffice2@gmail.com>
2021-12-05 20:31:53 -0300

    Tela de autenticação 2

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,37 +1,67 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/Authinput";
+import { IconeAtencao } from "../components/Icons";
 
 
 export default function Autenticacao(){
 
     const [modo, setModo]=useState<"login" | "cadastro">("login")
+    const [erro,setErro] = useState(null)
     const [email,setEmail] = useState("")
     const [senha,setSenha] = useState("")
+
+    function exibirErro(msg, tempoEMSegundos=5){
+        setErro(msg)
+        setTimeout(()=> setErro(null), tempoEMSegundos * 1000)
+    }
 
         function submeter(){
             if(modo === "login"){
                 console.log("Login")
+                
             }else{
                 console.log("cadastrar")
+                
             }
         }
 
 
     return(
         <div className={`
-        flex flex-col h-sreen items-center justify-center
+        flex h-sreen items-center justify-center
         `}>
+            <div className={`hidden md:block md:w-1/2 lg:w-2/3`}>
+                <img 
+                src="https://source.unsplash.com/random" 
+                alt="imagem da Tela de Autenticação" 
+                className={`h-screen w-full object-cover`}
+                />
+            </div>
         
         
 
-        <div className={`
-        w-1/2
+        <div className={`m-10
+        w-full mdw-1/2 lg:w-1/3
         `}>
             <h1 className={`
-            text-xl font-bold mb-5`}>
+            text-3xl font-bold mb-5`}>
                 {modo === "login" ? "Entre com Sua Conta" : "Cadastre-se na Plataforma"}
 
             </h1>
+
+            {erro ? (
+            <div className={`
+            flex items-end
+            bg-red-400 text-white py-3 px-5 my-2
+            border border-red-700 rounded-lg
+            `}>
+                {IconeAtencao()}
+                <span className={`ml-3`}>{erro}</span>
+            </div>
+            ) : false}
+
+
+
             <AuthInput 
             label="Email"
             tipo="email"
@@ -64,6 +94,27 @@ export default function Autenticacao(){
             
 
             </button>
+
+            {modo === 'login' ? (
+                <p className={`
+                mt-8
+                `}>Novo por aqui?
+                    <a onClick={()=>setModo('cadastro')} className={`
+                    text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                    `}> Criar uma Conta Gratuitamente
+                    </a>
+                </p>
+            ) : (
+                <p className={`
+                mt-8
+                `}>Ja faz parte da nossa comunidade?
+                    <a onClick={()=>setModo('login')} className={`
+                    text-blue-500 hover:text-blue-700 font-semibold cursor-pointer
+                    `}> Entre com a sua Credenciais
+                    </a>
+                </p>
+            )}
+
         </div>
         </div>
     )
